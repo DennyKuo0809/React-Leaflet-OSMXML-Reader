bf2597782415e5e6a8eb59e8d45a77afbd968c8d
DennyKuo0809 <ktying8989@gmail.com>
2023-11-21 18:55:23 +0800

    1st version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import MapWrapper from './MapComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Leaflet Map</h1>
+      <MapWrapper xmlFile="lanelet2_map.osm" center={[35.23808753540768, 139.9009591876285]}/>
     </div>
   );
 }
